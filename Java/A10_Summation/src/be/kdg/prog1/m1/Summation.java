5458194ab8e2a409a409eb3f4e6f3846d0a75753
Mihnea Ionita <dev@imihnea.com>
2020-09-26 11:39:50 +0200

    Updated summation assignment

--- Java/A10_Summation/src/be/kdg/prog1/m1/Summation.java
+++ Java/A10_Summation/src/be/kdg/prog1/m1/Summation.java
@@ -4,21 +4,37 @@ import java.util.Scanner;
 
 public class Summation {
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        int sum = 0;
-        int number = 1;
+        Scanner Keyboard = new Scanner(System.in);
+        Boolean Decision = true;
 
-        while(number != 0) {
-            System.out.print("Enter a number (to stop enter 0): ");
-            if(keyboard.hasNextInt()) {
-                number = keyboard.nextInt();
-                sum = sum + number;
-            } else {
-                System.out.println("Sir that's not a number.");
-                System.exit(0);
+        while(Decision) {
+            int Sum = 0;
+            int Number = 1;
+            int TotalNumbers = 0;
+
+            while (Number != 0) {
+                System.out.print("Enter a number (to stop enter 0): ");
+                if (Keyboard.hasNextInt()) {
+                    Number = Keyboard.nextInt();
+                    Sum = Sum + Number;
+                    if (Number != 0) TotalNumbers++;
+                } else {
+                    System.out.println("Sir, that's not a number.");
+                    System.exit(0);
+                }
             }
+
+            System.out.println("-----------------------------------");
+            System.out.println("The sum of these numbers is: " + Sum);
+            System.out.println("Total numbers entered: " + TotalNumbers);
+            System.out.println("-----------------------------------");
+
+            System.out.print("Would you like to calculate a new summation? ");
+            String UserDecision = Keyboard.next();
+            if(!UserDecision.equals("yes")) Decision = false;
         }
 
-        System.out.println("The sum of these numbers is: " + sum);
+        System.out.println("Program terminated.");
+        System.exit(0);
     }
 }
